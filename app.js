0010e397e25cb2e939f28fd1293cc373c658e4d9
elmir mammadli <elmir.memmedov6071@gmail.com>
2024-10-15 11:50:05 +0000

    AppJs

--- app.js
+++ app.js
@@ -5,11 +5,11 @@ $(document).ready(function () {
         e.preventDefault();
         if (!$.trim($('input').val()) == '') {
             let a = b => {
-                for (let i = 0; i < b; i++) { $('.minibox').append(`<h2>${b}</h2>`); }
-            }
+                for (let i = 0; i < b; i++) { $('.minibox').append(`<h2>${b}</h2>`); };
+            };
             let v = f => Number(f) ? a(f) : $('.minibox').append(`<h2>${f}</h2>`);
             v($('input').val());
         }
         $('input').val('');
-    })
+    });
 })
